899849a212f1d04c303e563076a4618028ee51aa
olyver <demar.07@gmail.com>
2017-07-10 00:30:20 +0700

    small changes

--- src/main/java/imageloader/imageloader/Controllers/HomeController.java
+++ src/main/java/imageloader/imageloader/Controllers/HomeController.java
@@ -4,23 +4,18 @@ package imageloader.imageloader.Controllers;
 import imageloader.imageloader.Entities.Image;
 import imageloader.imageloader.Services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Controller
 public class HomeController {
@@ -39,6 +34,7 @@ public class HomeController {
     public String index(Model model, Pageable pageable) {
         final Page<Image> page = imageService.findPage(pageable);
         model.addAttribute("page", page);
+//        if(page.hasPrevious())
         return "index";
     }
 
