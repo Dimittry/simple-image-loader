7356dcf7b4dbc1826c9d8c52e080e329b8f826ae
olyver <demar.07@gmail.com>
2017-07-10 12:47:51 +0700

    pom for wildfly

--- src/main/java/imageloader/imageloader/ImageLoaderApplication.java
+++ src/main/java/imageloader/imageloader/ImageLoaderApplication.java
@@ -2,11 +2,20 @@ package imageloader.imageloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ImageLoaderApplication {
+public class ImageLoaderApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImageLoaderApplication.class, args);
+		SpringApplication.run(applicationClass, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+	    return applicationBuilder.sources(ImageLoaderApplication.class);
+    }
+
+    private static Class<ImageLoaderApplication> applicationClass = ImageLoaderApplication.class;
 }
